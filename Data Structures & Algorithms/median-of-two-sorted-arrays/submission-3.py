9ec53754987755396fa1ea19839e25fe9ffb9274
Neil Purohit <58278838+NeilP06@users.noreply.github.com>
2026-07-20 22:13:50 -0500

    Add: median-of-two-sorted-arrays - submission-3

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A = nums1 if len(nums1) <= len(nums2) else nums2
+        B = nums1 if len(nums1) > len(nums2) else nums2
+        half = (len(nums1) + len(nums2)) // 2 if (len(nums1) + len(nums2)) % 2 == 0 else (len(nums1) + len(nums2)) // 2 + 1
+        res = 0.0
+
+        lo_A, hi_A = 1, len(A)
+        max_left, min_right = 0, 0
+
+        while lo_A <= hi_A:
+            mid_A = lo_A + (hi_A - lo_A) // 2
+            mid_B = half - mid_A
+
+            if mid_B != len(B) and A[mid_A - 1] > B[mid_B]:
+                hi_A = mid_A - 1
+            elif mid_A != len(A) and B[mid_B - 1] > A[mid_A]:
+                lo_A = mid_A + 1
+            else:
+                break
+        
+        mid_A = lo_A + (hi_A - lo_A) // 2
+        mid_B = half - mid_A
+        max_left = max(A[mid_A - 1], B[mid_B - 1]) if mid_A != 0 else B[mid_B - 1]
+        min_right = min(A[mid_A], B[mid_B]) if mid_A != len(A) else B[mid_B]
+        print(mid_A, mid_B)
+
+        if (len(nums1) + len(nums2)) % 2 == 1:
+            res = min(max_left, min_right)
+        else:
+            res = (max_left + min_right) / 2
+
+
+        return res
