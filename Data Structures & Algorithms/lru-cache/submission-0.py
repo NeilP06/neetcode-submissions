a26e63d83ea558a750c4cab8036dabb00cf6b1fa
Neil Purohit <58278838+NeilP06@users.noreply.github.com>
2026-08-05 14:29:22 -0500

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache[key]
+        self.cache[key] = val
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if self.size < self.capacity and key not in self.cache:
+            self.size = self.size + 1
+            self.cache[key] = value
+        elif key in self.cache:
+            self.cache[key] = value
+        else:
+            self.cache.popitem(last=False)
+            self.cache[key] = value
